aca7228252d5d97f6fac810fd92fe5666e267efd
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-26 23:42:15 +0300

    add log

--- src/main/java/com/smagin/exchange/exchange/service/OrderService.java
+++ src/main/java/com/smagin/exchange/exchange/service/OrderService.java
@@ -2,12 +2,14 @@ package com.smagin.exchange.exchange.service;
 
 import com.smagin.exchange.exchange.model.Order;
 import com.smagin.exchange.exchange.repository.OrderRepository;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Log
 public class OrderService {
     @Autowired
     private OrderRepository repository;
