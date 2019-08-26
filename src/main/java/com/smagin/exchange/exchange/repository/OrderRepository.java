aa699e0c566b028d386e1866f724839b29333b86
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-08-26 23:40:59 +0300

    add findById

--- src/main/java/com/smagin/exchange/exchange/repository/OrderRepository.java
+++ src/main/java/com/smagin/exchange/exchange/repository/OrderRepository.java
@@ -2,11 +2,15 @@ package com.smagin.exchange.exchange.repository;
 
 import com.smagin.exchange.exchange.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAll();
+
+    Optional<Order> findById(@Param("id") Long id);
 }
