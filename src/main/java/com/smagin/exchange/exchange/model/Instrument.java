475ce61f242432a689bb3ec071a6f0df8c375e0b
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-01 20:21:55 +0300

    add new empty

--- src/main/java/com/smagin/exchange/exchange/model/Instrument.java
+++ src/main/java/com/smagin/exchange/exchange/model/Instrument.java
@@ -3,12 +3,14 @@ package com.smagin.exchange.exchange.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Entity(name = "Instrument")
 @Data
 public class Instrument {
     @Id
+    @NotEmpty
     private Long id;
 
     @Column(name = "STATUS_BLOCKING")
