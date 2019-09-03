a3d6aaae0e90f9fe4052f9410082f384f157509e
Evgeniy Smagin <zhenyasmagin@yandex.ru>
2019-09-03 19:06:16 +0300

    add configurer

---
+++ src/main/java/com/smagin/exchange/exchange/Config.java
@@ -0,0 +1,26 @@
+package com.smagin.exchange.exchange;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.XmlBeamHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"com.smagin.exchange.exchange"})
+public class Config implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new XmlBeamHttpMessageConverter());
+    }
+    private HttpMessageConverter<Object> createXmlHttpMessageConverter(){
+        MarshallingHttpMessageConverter  converter = new MarshallingHttpMessageConverter();
+        return converter;
+    }
+}
