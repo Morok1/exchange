938dfb94e08cac76f9b884ca8bbe3a8987c57a94
u16681981 <u16681981@cab-wsm-0001690.sigma.sbrf.ru>
2019-09-03 17:25:31 +0300

    add exception handler

--- src/main/java/com/smagin/exchange/exchange/controller/InstrumentController.java
+++ src/main/java/com/smagin/exchange/exchange/controller/InstrumentController.java
@@ -2,13 +2,16 @@ package com.smagin.exchange.exchange.controller;
 
 import com.smagin.exchange.exchange.dto.InstrumentDTO;
 import com.smagin.exchange.exchange.service.InstrumentService;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Log
 public class InstrumentController {
     private InstrumentService service;
 
@@ -19,5 +22,10 @@ public class InstrumentController {
     @GetMapping("/instruments")
     public List<InstrumentDTO> findAll(){
         return service.findAll();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public void handleException(Exception e){
+        log.info("Cause of exception " + e.getCause());
     }
 }
